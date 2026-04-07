686de58613667950f0dc609bbbc756c23ed35245
federico-messineo-dev <federico.messineo.ai@gmail.com>
2026-04-07 21:30:31 +0200

    fix: single router with basename

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import About from './pages/About';
@@ -17,18 +17,16 @@ import { LanguageProvider } from './context/LanguageContext';
 export default function App() {
   return (
     <LanguageProvider>
-      <BrowserRouter>
-        <ScrollToTop />
-        <Routes>
-          <Route path="/" element={<Layout />}>
-            <Route index element={<Home />} />
-            <Route path="chi-sono" element={<About />} />
-            <Route path="servizi" element={<Services />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="contatti" element={<Contact />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="chi-sono" element={<About />} />
+          <Route path="servizi" element={<Services />} />
+          <Route path="portfolio" element={<Portfolio />} />
+          <Route path="contatti" element={<Contact />} />
+        </Route>
+      </Routes>
     </LanguageProvider>
   );
 }
